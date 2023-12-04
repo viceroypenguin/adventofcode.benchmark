e07876c4ef427b22c6302c039e3e148e3e51b522
Stuart Turner <stuart@turner-isageek.com>
2023-12-04 17:55:05 -0600

    Add @TwilightVanish 2023-04

---
+++ AdventOfCode.Common/Utility/CustomParser.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Common.Utility;
+
+public static class CustomParser
+{
+	public static int ParseInt(string input)
+	{
+		var value = 0;
+		for (var i = 0; i < input.Length; i++) value = value * 10 + (input[i] - '0');
+		return value;
+	}
+
+	public static int ParseInt(ReadOnlySpan<char> input)
+	{
+		var value = 0;
+		for (var i = 0; i < input.Length; i++) value = value * 10 + (input[i] - '0');
+		return value;
+	}
+
+	public static int CheckedParseInt(string input)
+	{
+		var value = 0;
+		for (var i = 0; i < input.Length; i++)
+		{
+			if (input[i] is < '0' or > '9') break;
+			value = value * 10 + (input[i] - '0');
+		}
+
+		return value;
+	}
+
+	public static int GreedyParseInt(ReadOnlySpan<char> input)
+	{
+		var value = 0;
+		for (var i = 0; i < input.Length; i++)
+		{
+			if (input[i] is < '0' or > '9') continue;
+			value = value * 10 + (input[i] - '0');
+		}
+
+		return value;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.AggressiveInlining)]
+	public static int SignedParseInt(ReadOnlySpan<char> input, int offset)
+	{
+		var negative = false;
+		var start = offset;
+
+		if (input[offset] == '-')
+		{
+			negative = true;
+			start++;
+		}
+
+		var value = 0;
+		for (var i = start; i < input.Length; i++)
+		{
+			value = value * 10 + (input[i] - '0');
+		}
+
+		return negative ? -value : value;
+	}
+}
---
+++ AdventOfCode.Puzzles/2023/day04.lisa.cs
@@ -0,0 +1,97 @@
+﻿using AdventOfCode.Common.Utility;
+
+namespace AdventOfCode.Puzzles._2023;
+
+[Puzzle(2023, 04, CodeType.Lisa)]
+public partial class Day_04_Lisa : IPuzzle
+{
+	private Scratcher[] _scratchers = default!;
+
+	public (string, string) Solve(PuzzleInput input)
+	{
+		_scratchers = ParseScratchers(input);
+
+		var part1 = GetScratcherPoints();
+		var part2 = GetScratcherCount();
+
+		return (part1.ToString(), part2.ToString());
+	}
+
+	private int GetScratcherPoints()
+	{
+		var points = 0;
+
+		for (var i = 0; i < _scratchers.Length; i++)
+		{
+			var scratcher = _scratchers[i];
+			var currentPoints = 0;
+
+			for (var j = 0; j < scratcher.ScratchedNumbers.Length; j++)
+			{
+				if (!scratcher.WinningNumbers.Contains(scratcher.ScratchedNumbers[j])) continue;
+				if (currentPoints == 0) currentPoints = 1;
+				else currentPoints *= 2;
+			}
+
+			points += currentPoints;
+		}
+
+
+		return points;
+	}
+
+	private int GetScratcherCount()
+	{
+		var scratcherCount = 0;
+
+		for (var i = 0; i < _scratchers.Length; i++)
+		{
+			var scratcher = _scratchers[i];
+			var currentWins = 0;
+
+			for (var j = 0; j < scratcher.ScratchedNumbers.Length; j++)
+			{
+				if (!scratcher.WinningNumbers.Contains(scratcher.ScratchedNumbers[j])) continue;
+				currentWins++;
+				_scratchers[i + currentWins].Count += scratcher.Count;
+			}
+
+			scratcherCount += scratcher.Count;
+		}
+
+		return scratcherCount;
+	}
+
+	private static Scratcher[] ParseScratchers(PuzzleInput input)
+	{
+		var scratchers = new Scratcher[input.Lines.Length];
+
+		var index = 0;
+		foreach (var line in input.Text.AsSpan().EnumerateLines())
+		{
+			if (line.Length == 0)
+				break;
+
+			var scratched = new int[10];
+			var winning = new int[25];
+
+			ParseWithOffset(scratched, line, 10);
+			ParseWithOffset(winning, line, 42);
+
+			scratchers[index] = new Scratcher(scratched, winning);
+			index++;
+		}
+
+		return scratchers;
+	}
+
+	private static void ParseWithOffset(int[] targetArray, ReadOnlySpan<char> line, int startIndex)
+	{
+		for (var i = 0; i < targetArray.Length; i++)
+		{
+			targetArray[i] = CustomParser.GreedyParseInt(line.Slice(startIndex + 3 * i, 2));
+		}
+	}
+
+	private record struct Scratcher(int[] ScratchedNumbers, int[] WinningNumbers, int Count = 1);
+}
