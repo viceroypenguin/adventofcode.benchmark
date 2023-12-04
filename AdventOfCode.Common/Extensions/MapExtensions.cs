4e317234a5987573c8648bcfed8040396584c332
Stuart Turner <stuart@turner-isageek.com>
2023-12-04 09:27:16 -0600

    Add @viceroypenguin 2023-01

---
+++ AdventOfCode.Common/Extensions/MapExtensions.cs
@@ -0,0 +1,96 @@
+﻿using SuperLinq;
+
+namespace AdventOfCode.Common.Extensions;
+
+public static class MapExtensions
+{
+	public static byte[][] GetMap(this byte[] input) =>
+		input.Segment(b => b == '\n')
+			.Select(l => l
+				.SkipWhile(b => b == '\n')
+				.ToArray())
+			.Where(l => l.Length > 0)
+			.ToArray();
+
+	public static int[][] GetIntMap(this byte[] input) =>
+		input.Segment(b => b == '\n')
+			.Select(l => l
+				.SkipWhile(b => b == '\n')
+				.Select(b => b - '0')
+				.ToArray())
+			.Where(l => l.Length > 0)
+			.ToArray();
+
+	public static IEnumerable<((int x, int y) p, T item)> GetMapPoints<T>(
+			this IReadOnlyList<IReadOnlyList<T>> map) =>
+		 Enumerable.Range(0, map.Count)
+			.SelectMany(y => Enumerable.Range(0, map[y].Count)
+				.Select(x => ((x, y), map[y][x])));
+
+	public static readonly IReadOnlyList<(int x, int y)> Neighbors =
+		new (int x, int y)[] { (0, 1), (0, -1), (1, 0), (-1, 0), };
+	public static IEnumerable<(int x, int y)> GetCartesianNeighbors(this (int x, int y) p) =>
+		Neighbors.Select(d => (p.x + d.x, p.y + d.y));
+
+	public static IEnumerable<(int x, int y)> GetCartesianNeighbors<T>(
+			this (int x, int y) p,
+			IReadOnlyList<IReadOnlyList<T>> map) =>
+		p.GetCartesianNeighbors()
+			.Where(q =>
+				q.y >= 0 && q.y < map.Count
+				&& q.x >= 0 && q.x < map[q.y].Count);
+
+	public static readonly IReadOnlyList<(int x, int y)> Adjacent =
+		new (int x, int y)[] { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1), };
+	public static IEnumerable<(int x, int y)> GetCartesianAdjacent(this (int x, int y) p) =>
+		Adjacent.Select(d => (p.x + d.x, p.y + d.y));
+
+	public static IEnumerable<(int x, int y)> GetCartesianAdjacent<T>(
+			this (int x, int y) p,
+			IReadOnlyList<IReadOnlyList<T>> map) =>
+		p.GetCartesianAdjacent()
+			.Where(q =>
+				q.y >= 0 && q.y < map.Count
+				&& q.x >= 0 && q.x < map[q.y].Count);
+
+	public static void FloodFill<T>(
+			this IReadOnlyList<IReadOnlyList<T>> map,
+			(int x, int y) startingPoint,
+			Func<(int x, int y), T, bool> canVisitPoint,
+			Action<(int x, int y), T> visitPoint)
+	{
+		// keep track of where we've been
+		var seen = new HashSet<(int x, int y)>();
+
+		// get the neighboring points...
+		IEnumerable<(int x, int y)> Traverse((int x, int y) q)
+		{
+			var (x, y) = q;
+
+			// we've been here before
+			if (seen.Contains((x, y)))
+				// don't go anywhere
+				return Array.Empty<(int x, int y)>();
+
+			// on another type of border
+			if (!canVisitPoint(q, map[y][x]))
+				// don't go anywhere
+				return Array.Empty<(int x, int y)>();
+
+			// now we've been here for the first time
+			// remember this and increase size of the basin
+			_ = seen.Add(q);
+			visitPoint(q, map[y][x]);
+
+			// go in all four directions
+			return q.GetCartesianNeighbors(map);
+		}
+
+		// execute a BFS based on traverse method
+		SuperEnumerable
+			.TraverseBreadthFirst(
+				startingPoint,
+				Traverse)
+			.Consume();
+	}
+}
---
+++ AdventOfCode.Puzzles/2023/day01.vp.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode.Puzzles._2023;
+
+[Puzzle(2023, 01, CodeType.ViceroyPenguin)]
+public partial class Day_01_ViceroyPenguin : IPuzzle
+{
+	public (string, string) Solve(PuzzleInput input)
+	{
+		var part1 = 0;
+		var part2 = 0;
+		foreach (var l in input.Span.EnumerateLines())
+		{
+			if (l.Length == 0)
+				continue;
+
+			var firstDigitIndex = l.IndexOfAnyInRange((byte)'0', (byte)'9');
+			var lastDigitIndex = l.LastIndexOfAnyInRange((byte)'0', (byte)'9');
+
+			var tens = (l[firstDigitIndex] - '0') * 10;
+			var ones = l[lastDigitIndex] - '0';
+			part1 += tens + ones;
+
+			var span = l[..firstDigitIndex];
+			while (span.Length >= 3)
+			{
+				(tens, var success) = span[0] switch
+				{
+					(byte)'o' when span.StartsWith("one"u8) => (10, true),
+					(byte)'t' when span.StartsWith("two"u8) => (20, true),
+					(byte)'t' when span.StartsWith("three"u8) => (30, true),
+					(byte)'f' when span.StartsWith("four"u8) => (40, true),
+					(byte)'f' when span.StartsWith("five"u8) => (50, true),
+					(byte)'s' when span.StartsWith("six"u8) => (60, true),
+					(byte)'s' when span.StartsWith("seven"u8) => (70, true),
+					(byte)'e' when span.StartsWith("eight"u8) => (80, true),
+					(byte)'n' when span.StartsWith("nine"u8) => (90, true),
+					_ => (tens, false),
+				};
+				if (success) break;
+
+				span = span[1..];
+			}
+
+			span = l[(lastDigitIndex + 1)..];
+			while (span.Length >= 3)
+			{
+				(ones, var success) = span[^1] switch
+				{
+					(byte)'e' when span.EndsWith("one"u8) => (1, true),
+					(byte)'o' when span.EndsWith("two"u8) => (2, true),
+					(byte)'e' when span.EndsWith("three"u8) => (3, true),
+					(byte)'r' when span.EndsWith("four"u8) => (4, true),
+					(byte)'e' when span.EndsWith("five"u8) => (5, true),
+					(byte)'x' when span.EndsWith("six"u8) => (6, true),
+					(byte)'n' when span.EndsWith("seven"u8) => (7, true),
+					(byte)'t' when span.EndsWith("eight"u8) => (8, true),
+					(byte)'e' when span.EndsWith("nine"u8) => (9, true),
+					_ => (ones, false),
+				};
+				if (success) break;
+
+				span = span[..^1];
+			}
+
+			part2 += tens + ones;
+		}
+
+		return (part1.ToString(), part2.ToString());
+	}
+}
